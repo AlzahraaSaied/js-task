64fc8605eb5a0f00b2614ec2ae46ad46cd18750e
Alzahraa Saied <zahraasaied592@gmail.com>
2024-10-15 14:23:41 +0300

    new commit-ahead

---
+++ js-task/js/index.js
@@ -0,0 +1,17 @@
+function NewString() {
+    const str = prompt("Please enter a string of 3 or more characters:");
+
+    // handling the string characters (three or more)
+
+    if (str.length < 3) {
+      return "String length must be 3 or more.";
+    }
+    const lastThree = str.slice(-3);
+
+    return lastThree + str + lastThree;
+  }
+  
+  // Display the result to the user
+  alert(NewString());
+
+  
